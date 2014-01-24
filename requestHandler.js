a4adf78e5bb7ce4155e22555909267bf38399c75
liobouchan <leo.bouchan@gmail.com>
2014-01-24 00:42:35 -0600

    Comments and code organizated in requestHandler.js

--- requestHandler.js
+++ requestHandler.js
@@ -2,25 +2,26 @@ var querystring = require("querystring");
 var formidable = require("formidable");
 var fs = require ("fs");
  
-function start(response)
-{
- console.log("Request handler start was called.");
- var body = "<html>" +
- "<head>" +
- "<meta http-equiv='Content-Type' content='text/html'; " +
- "charset='UTF-8' />" +
- "</head>" +
- "<body>" +
- "<form action='/upload' enctype='multipart/form-data' method='post'>" +
- "<input type='file' name='upload'></input>" +
- "<input type='submit' value='Upload file' />" +
- "</form>" +
- "</body>" +
- "</html>" ;
- response.writeHead(200,{"Content-Type":"text/html"});
- response.write(body);
- response.end();
+function start(response){
+  //funcion que inicializa la parte gráfica de mi html FORMULARIO
+  console.log("Request handler start was called.");
+  var body = "<html>" +
+  "<head>" +
+  "<meta http-equiv='Content-Type' content='text/html'; " +
+  "charset='UTF-8' />" +
+  "</head>" +
+  "<body>" +
+  "<form action='/upload' enctype='multipart/form-data' method='post'>" +
+  "<input type='file' name='upload'></input>" +
+  "<input type='submit' value='Upload file' />" +
+  "</form>" +
+  "</body>" +
+  "</html>" ;
+  response.writeHead(200,{"Content-Type":"text/html"});
+  response.write(body);
+  response.end();
 }
+
 function upload(response,request)
 {
  console.log("Request handler upload was called.");
